7ddccd8274e8cfd28659b9725f58d531bb6cf06d
M-sandoval-93 <sandoval.mario.08@gmail.com>
2025-05-12 17:01:31 -0400

    feat: add footer and header with structure

---
+++ src/consts/menu.ts
@@ -0,0 +1,22 @@
+export const MENU = [
+    {
+        name: "Inicio",
+        path: "/", 
+    },
+    {
+        name: "Nuestro liceo",
+        path: "/", 
+    },
+    {
+        name: "Institucional",
+        path: "/", 
+    },
+    {
+        name: "Intranet",
+        path: "/", 
+    },
+    {
+        name: "Noticias",
+        path: "/", 
+    },
+]
